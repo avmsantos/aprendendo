8f2812b6562f1d3f06dcfc61ed88dc5773860f2f
Amanda Victoria <amandamelquiades01@gmail.com>
2022-01-28 19:08:44 -0300

    alterei a main

--- main.dart
+++ main.dart
@@ -3,14 +3,11 @@ import 'dart:math';
 void main() {
   final numPar = () => print('numero par');
   final numImpar = () => print('numero impar');
-
   executar(numPar, numImpar);
 }
 
 void executar(Function par, Function impar) {
-  if (Random().nextInt(40) % 2 == 0) {
-    par();
-  } else {
-    impar();
-  }
+  final aleatorio = Random().nextInt(43);
+  aleatorio % 2 == 0 ? par() : impar();
+  print('numero aleatorio foi $aleatorio');
 }
